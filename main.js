7ad4c95018c37f884c15f5ac096b3af2f611128c
ReubenBarboza <reuben.b@iksula.com>
2023-03-28 17:38:10 +0530

    Wrapped previous commit with a router alpine component

--- main.js
+++ main.js
@@ -3,4 +3,25 @@ import "./index.css";
 
 window.Alpine = Alpine;
 
+Alpine.data("router", () => ({
+  page: location.hash,
+  todoId: null,
+  routes: ["", "#login", "#todolist", "#create-todo"],
+  routesWithQueryParams: [/#todo\?id=[a-zA-Z0-9]+/],
+
+  setTodoId() {
+    const [hash, query] = this.page.split("?");
+    const todoId = new URLSearchParams(query).get("id");
+    this.todoId = todoId;
+  },
+
+  checkRoutesFor404() {
+    let foundANormalRoute = this.routes.includes(this.page);
+    let foundARouteWithQueryParams = this.routesWithQueryParams.some((regex) =>
+      regex.test(this.page)
+    );
+    return !(foundANormalRoute || foundARouteWithQueryParams);
+  },
+}));
+
 Alpine.start();
