1b7bf7d9498f0eeef266036f23c0b4182b5a12e9
ReubenBarboza <reuben.b@iksula.com>
2023-04-05 13:05:46 +0530

    Added edit form functionality

---
+++ components/editTodo.js
@@ -0,0 +1,78 @@
+export default () => ({
+  editTitle: "",
+  editDescription: "",
+
+  editTitleError: null,
+  editDescriptionError: null,
+
+  editTitleErrorMessage: "",
+  editDescriptionErrorMessage: "",
+
+  isLoading: null,
+
+  editTitleValidate(initialTitle) {
+    if (this.editTitle === "") {
+      this.editTitleError = true;
+      this.editTitleErrorMessage = "Title cannot be empty.";
+    } else {
+      this.editTitleError = null;
+      this.editTitleErrorMessage = "";
+    }
+  },
+
+  editDescriptionValidate(initialDescription) {
+    if (this.editDescription === initialDescription) {
+      this.editDescriptionError = true;
+      this.editDescriptionErrorMessage =
+        "Description cannot be the same as before.";
+    } else if (this.editDescription === "") {
+      this.editDescriptionError = true;
+      this.editDescriptionErrorMessage = "Description cannot be empty.";
+    } else {
+      this.editDescriptionError = null;
+      this.editDescriptionErrorMessage = "";
+    }
+  },
+
+  clearInputs() {
+    this.editTitle = "";
+    this.editDescription = "";
+  },
+
+  async updateDataInFirebase(data) {
+    const todoParams = Object.fromEntries(new URLSearchParams(location.hash));
+    todoParams["id"] = todoParams["#todo?id"];
+    try {
+      if (todoParams.id) {
+        await fetch(
+          `https://todo-collab-e32d9-default-rtdb.firebaseio.com/todo/${todoParams.id}.json`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      this.isLoading = false;
+    }
+  },
+
+  async submitHandler() {
+    this.editTitleValidate();
+    this.editDescriptionValidate();
+    if (!this.editTitleError && !this.editDescriptionError) {
+      await this.updateDataInFirebase({
+        createdAt: new Date(),
+        title: this.editTitle,
+        description: this.editDescription,
+      });
+      this.$dispatch("todo-updated");
+      location.href = "main.html#sticky-wall";
+    }
+  },
+});
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./index.css";
 import loginValidation from "./components/loginValidation";
 import createNewTodo from "./components/createNewTodo";
 import todo from "./components/todo";
+import editTodo from "./components/editTodo";
 
 window.Alpine = Alpine;
 
@@ -11,5 +12,7 @@ Alpine.data("loginValidation", loginValidation);
 Alpine.data("createNewTodo", createNewTodo);
 
 Alpine.data("todo", todo);
+
+Alpine.data("editTodo", editTodo);
 
 Alpine.start();
